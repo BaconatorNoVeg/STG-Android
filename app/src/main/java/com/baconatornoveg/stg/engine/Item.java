4576dddea34590d02356ec17fae34001554a500f
Joshua Luce <joshuabluce@gmail.com>
2018-07-25 18:15:05 -0500

    Starting engine upgrade

--- app/src/main/java/com/baconatornoveg/stg/engine/Item.java
+++ app/src/main/java/com/baconatornoveg/stg/engine/Item.java
@@ -3,14 +3,30 @@ package com.baconatornoveg.stg.engine;
 public class Item {
 
     private String name;
+    private String itemType;
+    private boolean isPhysical;
+    private boolean isMagical;
 
-    public boolean isPhysical;
-    public boolean isMagical;
 
-    public Item(boolean isPhysical, boolean isMagical, String name) {
+    Item(boolean isPhysical, boolean isMagical, String itemType, String name) {
+        this.name = name;
+        this.itemType = itemType;
+        this.isPhysical = isPhysical;
+        this.isMagical = isMagical;
+    }
+
+    Item(boolean isPhysical, boolean isMagical, String name) {
         this.name = name;
         this.isPhysical = isPhysical;
         this.isMagical = isMagical;
+    }
+
+    public boolean isPhysical() {
+        return isPhysical;
+    }
+
+    public boolean isMagical() {
+        return isMagical;
     }
 
     public String toString() {
