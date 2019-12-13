0637ef235ed7982306c1190e496d895881d23cf4
Joshua Luce <joshuabluce@protonmail.com>
2019-12-12 23:22:36 -0600

    Optimize imports

--- app/src/main/java/com/baconatornoveg/stg/AppInfoActivity.java
+++ app/src/main/java/com/baconatornoveg/stg/AppInfoActivity.java
@@ -5,16 +5,13 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
 
-import com.baconatornoveg.stg.database.BuildDatabase;
 import com.baconatornoveg.stglib.SmiteTeamGenerator;
 
 public class AppInfoActivity extends AppCompatActivity {
